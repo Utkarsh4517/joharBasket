cf2f4d034062b7ca946893c99eeba5fc5cda944a
Utkarsh4517 <uttkarsh.dav@gmail.com>
2023-09-29 15:39:01 +0530

    added local notification when the app is in foreground

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:johar/Routes/route_generator.dart';
 import 'package:johar/firebase_options.dart';
@@ -13,10 +14,15 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   await FirebaseMessaging.instance.requestPermission();
+  await _initLocalNotifications();
+
+
   FirebaseMessaging.onMessage.listen((RemoteMessage message) {
     print('Got a message whilst in the foreground!');
     print('Message data: ${message.data}');
     print(message.data['body']);
+
+    _showLocalNotification(message);
 
 
     if (message.notification != null) {
@@ -27,6 +33,38 @@ void main() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   bool hasSeenOnboarding = prefs.getBool('hasSeenOnboarding') ?? false;
   runApp(ProviderScope(child: MyApp(hasSeenOnboarding: hasSeenOnboarding)));
+}
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+Future<void> _initLocalNotifications() async {
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+  final InitializationSettings initializationSettings =
+      InitializationSettings(android: initializationSettingsAndroid);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+}
+
+void _showLocalNotification(RemoteMessage message) async {
+  const AndroidNotificationDetails androidPlatformChannelSpecifics =
+      AndroidNotificationDetails(
+    'johar', // Change this to a unique channel ID
+    'Your Channel Name',
+    channelDescription: '',
+    importance: Importance.max,
+    priority: Priority.high,
+  );
+  const NotificationDetails platformChannelSpecifics =
+      NotificationDetails(android: androidPlatformChannelSpecifics);
+
+  await flutterLocalNotificationsPlugin.show(
+    0,
+    message.data['title'],
+    message.data['body'],
+    platformChannelSpecifics,
+    payload: 'item x',
+  );
 }
 
 class MyApp extends StatelessWidget {
